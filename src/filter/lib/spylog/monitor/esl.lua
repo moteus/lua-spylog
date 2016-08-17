ccfac4fe0b63dd2be07d5de79b000dd4f7c1621c
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-08-17 10:21:24 +0300

    Fix. ESL url with default auth. Add. Check ESL log level

--- src/filter/lib/spylog/monitor/esl.lua
+++ src/filter/lib/spylog/monitor/esl.lua
@@ -3,10 +3,32 @@ local ut     = require "lluv.utils"
 local esl    = require "lluv.esl"
 local log    = require "spylog.log"
 
+local FS_LOG_LEVELS_NAMES = {
+  ['0'] = "CONSOLE";
+  ['1'] = "ALERT";
+  ['2'] = "CRIT";
+  ['3'] = "ERR";
+  ['4'] = "WARNING";
+  ['5'] = "NOTICE";
+  ['6'] = "INFO";
+  ['7'] = "DEBUG";
+}
+
+local FS_LOG_LEVELS = {
+  CONSOLE = '0';
+  ALERT   = '1';
+  CRIT    = '2';
+  ERR     = '3';
+  WARNING = '4';
+  NOTICE  = '5';
+  INFO    = '6';
+  DEBUG   = '7';
+}
+
 local function esl_monitor(endpoint, opt, cb)
   local auth, address, port = ut.split_first(endpoint, "@", true)
   if not address then
-    auth, address = 'ClueCon', address
+    auth, address = 'ClueCon', auth
   end
 
   address, port = ut.split_first(address, ":", true)
@@ -15,7 +37,9 @@ local function esl_monitor(endpoint, opt, cb)
   local log_header = string.format('[esl] [%s:%d]', address, port)
 
   local reconnect_timeout = (opt and opt.reconnect or 30) * 1000
-  local level             = opt and opt.level or 'WARNING'
+  local level, level_name = string.upper(tostring(opt and opt.level or 'WARNING'))
+  level = FS_LOG_LEVELS[level] or level
+  level_name = assert(FS_LOG_LEVELS_NAMES[level], 'Unknon log level: ' .. level)
 
   local reconnect_timer = uv.timer(0)
 
@@ -32,7 +56,7 @@ local function esl_monitor(endpoint, opt, cb)
 
       log.info("%s connected", log_header)
 
-      self:log(level, function(self, err, event)
+      self:log(level_name, function(self, err, event)
         if err then return end
         local ok, status, msg = event:getReply()
         log.info('%s log command: %s %s', log_header, tostring(status), tostring(msg))
@@ -58,7 +82,7 @@ local function esl_monitor(endpoint, opt, cb)
 
 end
 
-local function els_filter(filter, event)
+local function esl_filter(filter, event)
   -- local file    = event:getHeader('Log-File')
   -- local line    = event:getHeader('Log-Line')
   -- local func    = event:getHeader('Log-Func')
@@ -76,5 +100,5 @@ end
 
 return {
   monitor = esl_monitor;
-  filter = els_filter;
+  filter = esl_filter;
 }
