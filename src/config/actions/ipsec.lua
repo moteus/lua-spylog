aace88176d5272bf2ef59cdd7e20ff3fd52354ea
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-08-17 17:24:31 +0300

    Add. Some comments to describe how to install.

--- src/config/actions/ipsec.lua
+++ src/config/actions/ipsec.lua
@@ -1,3 +1,12 @@
+-- Start action:
+--    netsh ipsec static delete policy name=Block
+--    netsh ipsec static add filteraction name=Block action=block
+--    netsh ipsec static add filter filterlist=BlockList srcaddr=192.168.192.100 dstaddr=me
+--    netsh ipsec static add policy name=Block assign=yes activatedefaultrule=no
+--    netsh ipsec static add rule name=BlockList policy=Block filterlist=BlockList filteraction=Block
+--    netsh ipsec static delete filter filterlist=BlockList srcaddr=192.168.192.100 dstaddr=Me
+-- Stop action:
+--    netsh ipsec static delete policy name=Block
 ACTION{"ipsec",
   ban   = { "netsh", "ipsec static add filter filterlist=BlockList srcaddr=<HOST> dstaddr=me description=\"<DATE> <FILTER> <JAIL> <BANTIME>\""};
   unban = { "netsh", "ipsec static delete filter filterlist=BlockList srcaddr=<HOST> dstaddr=me"};
--- src/config/filters/rdp.lua
+++ src/config/filters/rdp.lua
@@ -1,3 +1,23 @@
+-- Configure system
+-- 1. Install SNMP Service
+--  * Dism.exe /online /enable-feature /featurename:SNMP
+--  * Dism.exe /online /enable-feature /featurename:"WMISnmpProvider"
+-- 2. Go to Services->`SNMP Service`
+-- 3. Go to Traps tab
+-- 3.1 Add `public` to Community list
+-- 3.2 Add `127.0.0.1` to trap destinations
+-- 4. Go to Security tab
+-- 4.1 Check send authentications trap
+-- 4.2 Add `public`/`READ ONLY` to accepted community
+-- 5. Run `evntwin`
+-- 5.1 Check `Custom` configuration
+-- 5.2 Press `Edit>>>`
+-- 5.3 Select `Security/Security`
+-- 5.4 Add events 529 or 4625 (depend on Windows version)
+-- 6. Run `gpedit.msc`
+-- 6.1 Go to Computer / Windows Settings / Security Settings / Local Policies / Audit Policy
+-- 6.2 Set `Audit account logon`  events = Success, Failure
+
 FILTER{ "rdp-fail-access";
   enabled = false;
   source  = "trap:udp://127.0.0.1";
