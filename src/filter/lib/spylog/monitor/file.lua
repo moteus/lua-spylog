f925152bf5deea91b835e72304802dc1ef5c0acd
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-08-18 13:40:25 +0300

    Add. Check if log line too long in file.

--- src/filter/lib/spylog/monitor/file.lua
+++ src/filter/lib/spylog/monitor/file.lua
@@ -4,22 +4,33 @@ local date      = require "date"
 local log       = require "spylog.log"
 local filemon   = require "spylog.filemon"
 
+local MAX_LINE_LENGTH = 4096
+
 local unpack = unpack or table.unpack
 
 local function file_monitor(fname, opt, cb)
+  local log_header = string.format('[file:%s]', fname)
+
   local eol = (opt or {}).eol or {'\r*\n', true}
   if type(eol) == 'string' then eol = {eol, false} end
   local buffer = ut.Buffer.new(unpack(eol, 1, 2))
   local monitor = filemon.new(opt)
+
   monitor:open(fname, function(self, err, data)
     if err then 
-      return log.error("READ FILE: %s", tostring(err))
+      return log.error("%s READ FILE: %s", log_header, tostring(err))
     end
 
     buffer:append(data)
     while true do
       local line = buffer:read_line()
-      if not line then break end
+      if not line then
+        if buffer.size and (buffer:size() > MAX_LINE_LENGTH) then
+          log.alert('%s get too long line: %d `%s...`', log_header, buffer:size(), buffer:read_n(256))
+          buffer:reset()
+        end
+        break
+      end
       cb(line)
     end
   end)
