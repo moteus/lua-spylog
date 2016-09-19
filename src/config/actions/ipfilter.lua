5fc0b0b5cb806c42023caea5d11f2004c5d8451f
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-09-19 14:43:29 +0300

    Remove unused action.

--- src/config/actions/ipfilter.lua
+++
@@ -1,8 +0,0 @@
-ACTION{"ipfilter",
-  ban   = { [[c:\ipf\ipfilter.exe]], "blacklistadd=<HOST>,255.255.255.255,<JAIL>"};
-  unban = { [[c:\ipf\ipfilter.exe]], "blacklistremove=<HOST>,255.255.255.255" };
-  unique = "ipfilter <HOST>";
-  options = {
-    timeout = 10000;
-  }
-}
