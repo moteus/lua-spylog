eeb1bef53a8aefba2e8be645fe38bc4abd0256fa
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-09-21 12:39:17 +0300

    Move part of code from filter/main.lua to separate file.

---
+++ src/filter/lib/spylog/filter/manager.lua
@@ -0,0 +1,132 @@
+local config = require "spylog.config"
+local log    = require "spylog.log"
+local path   = require "path"
+local uv     = require "lluv"
+local ut     = require "lluv.utils"
+
+local function append(t, v)
+  t[#t + 1] = v
+  return t
+end
+
+local Source = ut.class() do
+
+local function apply_filter(jail, filters, filter, ...)
+  for i = 1, #filters do
+    local capture = filter(filters[i], ...)
+    if capture then
+      jail(filters[i], capture)
+      if filters[i].stop then
+        break
+      end
+    end
+  end
+end
+
+-- @static
+function Source.decode_source(source)
+  if type(source) == 'string' then
+    source = config.SOURCES and config.SOURCES[source] or source
+  end
+
+  local source_string = (type(source) == 'table') and source[1] or source
+
+  assert(type(source_string) == 'string', "source string required")
+
+  local source_type, source_info = ut.split_first(source_string, ':', true)
+
+  assert(source_info, string.format('invalid source string: %s', source_string))
+
+  if source_type == 'file' then
+    source_info   = path.fullpath(source_info)
+    source_string = source_type..":"..source_info
+    if type(source) == 'table' then source[1] = source_string end
+  end
+
+  return source_string, source_type, source_info, (type(source) == 'table') and source or nil
+end
+
+function Source:__init(source)
+  local source_string, source_type, source_info, source_opt = Source.decode_source(source)
+
+  log.info("create new source: `%s`", source_string)
+
+  self._string  = source_string
+  self._type    = source_type
+  self._info    = source_info
+  self._opt     = source_opt
+  self._filters = {}
+
+  return self
+end
+
+function Source:start(jail)
+  if not self._m then
+    log.info("start source monitor for: `%s`", self._string)
+
+    local m = require ("spylog.monitor." .. self._type)
+    local filters = self._filters
+    m.monitor(self._info, self._opt, function(...)
+      apply_filter(jail, filters, m.filter, ...)
+    end)
+
+    self._m = m
+  end
+end
+
+function Source:add(filter)
+  if self._type == 'trap' then
+    if type(filter.trap) ~= 'table' then
+      filter.trap = {[filter.trap] = true}
+    else
+      for i =1, #filter.trap do
+        filter.trap[filter.trap[i]] = true
+      end
+    end
+  end
+
+  log.info("attach filter `%s` to source `%s`", filter.name, self._string)
+
+  append(self._filters, filter)
+end
+
+end
+
+local FilterManager = ut.class() do
+
+function FilterManager:__init()
+  self._sources = {}
+
+  return self
+end
+
+function FilterManager:source(filter)
+  local source_string = Source.decode_source(filter.source)
+
+  local s = self._sources[source_string]
+  if not s then
+    s = Source.new(filter.source)
+    self._sources[source_string] = s
+  end
+
+  return s
+end
+
+function FilterManager:add(filter)
+  local source = self:source(filter)
+  source:add(filter)
+  return self
+end
+
+function FilterManager:start(jail)
+  local n = 0
+  for _, source in pairs(self._sources) do
+    source:start(jail)
+    n = n + 1
+  end
+  return n
+end
+
+end
+
+return FilterManager
--- src/filter/main.lua
+++ src/filter/main.lua
@@ -3,65 +3,15 @@ local config      = require "spylog.config"
 config.LOG.prefix = "[filter] "
 -------------------------------------------------
 
-local log       = require "spylog.log"
-local uv        = require "lluv"
-local ut        = require "lluv.utils"
-local zthreads  = require "lzmq.threads"
-local ztimer    = require "lzmq.timer"
-local cjson     = require "cjson.safe"
-local path      = require "path"
-local stp       = require "StackTracePlus"
-local filterrex = require "spylog.filter"
-local exit      = require "spylog.exit"
-
-local function build_filters(t)
-  for i = 1, #t do
-    local filter = t[i]
-    filter.name = filter.name or t[i][1]
-    filter.match = filterrex(filter)
-    local source = filter.source
-
-    if type(source) == 'string' then
-      source = config.SOURCES and config.SOURCES[source] or source
-    end
-
-    filter.source = source
-
-    if type(source) == 'table' then
-      source = source[1]
-    end
-
-    assert(type(source) == 'string', "source required")
-
-    local typ, src = ut.split_first(source, ':', true)
-
-    if typ == 'file' then
-      src = path.fullpath(src)
-      source = typ..":"..src
-      if type(filter.source) == 'table' then
-        filter.source[1] = source
-      else
-        filter.source = source
-      end
-    elseif typ == 'trap' then
-      if type(filter.trap) ~= 'table' then
-        filter.trap = {[filter.trap] = true}
-      else
-        for i =1, #filter.trap do
-          filter.trap[filter.trap[i]] = true
-        end
-      end
-    end
-  end
-  return t
-end
-
-local ok, FILTERS = pcall(build_filters, config.FILTERS)
-if not ok then
-  log.fatal("Can not build filter: %s", tostring(FILTERS))
-  ztimer.sleep(500)
-  return SERVICE.exit()
-end
+local log           = require "spylog.log"
+local uv            = require "lluv"
+local zthreads      = require "lzmq.threads"
+local ztimer        = require "lzmq.timer"
+local cjson         = require "cjson.safe"
+local stp           = require "StackTracePlus"
+local regex         = require "spylog.filter.regex"
+local FilterManager = require "spylog.filter.manager"
+local exit          = require "spylog.exit"
 
 local pub, err = zthreads.context():socket("PUB", {
   [config.CONNECTIONS.FILTER.JAIL.type] = config.CONNECTIONS.FILTER.JAIL.address
@@ -72,8 +22,6 @@ if not pub then
   ztimer.sleep(500)
   return SERVICE.exit()
 end
-
-log.info("Service start")
 
 log.debug("config.LOG.multithread: %s", tostring(config.LOG.multithread))
 
@@ -90,53 +38,23 @@ local function jail(filter, capture)
   pub:send(msg)
 end
 
-local function apply_filter(filters, filter, ...)
-  for i = 1, #filters do
-    local capture = filter(filters[i], ...)
-    if capture then
-      jail(filters[i], capture)
-      if filters[i].stop then
-        break
-      end
-    end
-  end
-end
-
-local function add_monitor(filters, typ, src, opt)
-  local m = require ("spylog.monitor." .. typ)
-  m.monitor(src, opt, function(...)
-    apply_filter(filters, m.filter, ...)
-  end)
-end
-
 local function init_service()
-  local sources = {}
-  for i = 1, #FILTERS do
-    local filter = FILTERS[i]
+  local filters = FilterManager.new()
+
+  for i = 1, #config.FILTERS do
+    local filter = config.FILTERS[i]
     if filter.enabled then
-      local source = filter.source
-      if type(source) == 'table' then
-        source = source[1]
-      end
+      filter.name  = filter.name or filter[1]
+      filter.match = regex(filter)
 
-      local typ, src = ut.split_first(source, ':', true)
+      assert(type(filter.name) == 'string', 'invalid filter name')
+      assert(filter.source, string.format('filter `%s` has no source', filter.name))
 
-      local monitor = sources[source]
-      if not monitor then
-        log.info("Start new monitor for `%s`", source)
-        local FILTER = {}
-        monitor = {FILTER = FILTER}
-        sources[source] = monitor
-        local opt = (type(filter.source) == 'table') and filter.source or nil
-        add_monitor(FILTER, typ, src, opt)
-      end
-
-      log.info("Add `%s` filter for `%s`", filter.name, source)
-      monitor.FILTER[#monitor.FILTER + 1] = filter
+      filters:add(filter)
     end
   end
 
-  if not next(sources) then
+  if 0 == filters:start(jail) then
     log.warning("there no active filters")
   end
 end
@@ -148,6 +66,8 @@ if not ok then
   ztimer.sleep(500)
   return SERVICE.exit()
 end
+
+log.info("Service start")
 
 exit.start_monitor(...)
 
