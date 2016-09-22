773056c4365b306e1bd82f4fd14ac189e254cc0b
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-09-22 13:01:47 +0300

    Add. `regex` prefilter.

---
+++ src/jail/lib/spylog/prefilter/regex.lua
@@ -0,0 +1,70 @@
+local ut         = require "lluv.utils"
+local BaseFilter = require "spylog.prefilter.base"
+
+local ENGINES = {
+  default = function(list)
+    local regex = {} for i = 1, #list do
+      assert(type(list[i]) == 'string')
+      regex[i] = list[i]
+    end
+
+    return function(s)
+      for i = 1, #regex do
+        if string.find(s, regex[i]) then
+          return true
+        end
+      end
+    end
+  end;
+
+  pcre = function(list)
+    local rex = require "rex_pcre"
+
+    local regex = {} for i = 1, #list do
+      regex[i] = assert(rex.new(list[i]))
+    end
+
+    return function(s)
+      for i = 1, #regex do
+        if regex[i]:find(s) then
+          return true
+        end
+      end
+    end
+  end;
+}
+
+local RegexFilter = ut.class(BaseFilter) do
+
+function RegexFilter:__init(filter)
+  assert(filter.regex, 'prefilter with type `regex` has no regex list')
+  if type(filter.regex) == 'string' then
+    filter.regex = {filter.regex}
+  end
+
+  assert(filter.value, 'prefilter with type `regex` has no value')
+
+  self.__base.__init(self, filter)
+
+  local engine = filter.engine or 'default'
+
+  engine = assert(ENGINES[engine], string.format('prefilter with type `regex` has unknown engine %s', tostring(engine)))
+
+  self._find = engine(filter.regex)
+
+  return self
+end
+
+function RegexFilter:apply(capture)
+  local value = self:value(capture)
+
+  if value and self._find(value) then
+    return self._allow
+  end
+
+  return not self._allow
+end
+
+end
+
+return RegexFilter
