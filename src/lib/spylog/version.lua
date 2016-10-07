4b0f718ab717d16828313226e28658a51fc937fa
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-10-07 12:42:51 +0300

    Release 0.0.1-beta.1

--- src/lib/spylog/version.lua
+++ src/lib/spylog/version.lua
@@ -1,6 +1,6 @@
 return {
   _NAME      = 'SpyLog';
-  _VERSION   = '0.0.1-dev';
+  _VERSION   = '0.0.1-beta.1';
   _COPYRIGHT = 'Copyright (C) 2015-2016 Alexey Melnichuk';
   _URL       = 'https://github.com/moteus/lua-spylog';
   _LICENSE   = 'MIT';
