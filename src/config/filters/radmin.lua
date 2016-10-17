3cdb14a1231e521afde471effdb66e143c9a40dd
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-10-17 11:52:48 +0300

    Add. RAdmin filter.

---
+++ src/config/filters/radmin.lua
@@ -0,0 +1,15 @@
+FILTER{ "radmin-fail-access";
+  enabled = false;
+  -- radmin 2.2
+  source  = "file:c:/logfile.txt";
+  -- radmin 3.0
+  -- source  = "file:C:/WINDOWS/system32/rserver30/Radm_log.htm";
+  exclude = WHITE_IP;
+  hint    = "Password is incorrect";
+  failregex = {
+    -- radmin 2.2
+    "^[%d+%.<>: ]+Connection from ([%d+%.]+) : Password is incorrect or error occurs"
+    -- radmin 3.0
+    "^&lt;%d+&gt; RServer3 [^()]+%(([%d%.]+)%).-Password is incorrect or error occurs"
+  };
+}
--- src/config/jails/access.lua
+++ src/config/jails/access.lua
@@ -1,7 +1,7 @@
 JAIL{"fail-access";
   enabled  = false;
-  filter   = {"rdp-fail-access", "fusionpbx-fail-access"};
-  findtime = 30;
+  filter   = {"rdp-fail-access", "fusionpbx-fail-access", "radmin-fail-access"};
+  findtime = 300;
   maxretry = 4;
   bantime  = 600;
   action   = "ipsec";
