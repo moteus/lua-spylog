6eb4f51929da713c7df375733e3502a21dc4ceb1
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2017-06-26 13:50:58 +0300

    Back to dev version

--- src/lib/spylog/version.lua
+++ src/lib/spylog/version.lua
@@ -3,7 +3,7 @@
 -- !!! Do not define more than one value per line
 return {
   _NAME      = 'SpyLog';
-  _VERSION   = '0.0.1';
+  _VERSION   = '0.0.2-dev';
   _COPYRIGHT = 'Copyright (C) 2015-2017 Alexey Melnichuk';
   _URL       = 'https://github.com/moteus/lua-spylog';
   _LICENSE   = 'MIT';
