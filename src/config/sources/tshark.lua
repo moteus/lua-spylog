d7c782609afaa2e37c35e8e79c873c56c0dd8e7b
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2017-07-11 17:44:33 +0300

    Fix. tshark process command.

--- src/config/sources/tshark.lua
+++ src/config/sources/tshark.lua
@@ -1,6 +1,6 @@
 SOURCE{"tshark-sip-request",
   'process:C:/Program Files/Wireshark/tshark.exe';
-  args = '-i 6 -f "port 5060" -o gui.column.format:"Time,%Yt" -T fields '
+  args = '-i 6 -f "port 5060" -o gui.column.format:Time,%Yt -T fields '
       .. '-e _ws.col.Time -e ip.src -e ip.dst -e sip.r-uri.host -e sip.r-uri.user -e sip.Request-Line '
       .. '-Y "sip.Request-Line != """""';
   restart = 5;
