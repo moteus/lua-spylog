04550a97fdbd89cfdb16e315291338181831bb21
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2017-07-11 13:53:47 +0300

    Update tshark sip example

--- src/config/sources/tshark.lua
+++ src/config/sources/tshark.lua
@@ -1,6 +1,8 @@
 SOURCE{"tshark-sip-request",
-  'process:"C:/Program Files/Wireshark/tshark.exe" -l -i 6 -f "port 5060" -T fields -e ip.src -e ip.dst -e sip.Request-Line -Y "sip.Request-Line != """""',
-  -- args = {}
+  'process:C:/Program Files/Wireshark/tshark.exe';
+  args = '-i 6 -f "port 5060" -o gui.column.format:"Time,%Yt" -T fields '
+      .. '-e _ws.col.Time -e ip.src -e ip.dst -e sip.r-uri.host -e sip.r-uri.user -e sip.Request-Line '
+      .. '-Y "sip.Request-Line != """""';
   restart = 5;
   monitor = 'stdout';
   eol = {'\r\n', false};
